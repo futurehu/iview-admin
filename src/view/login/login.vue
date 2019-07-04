74f127c45ef937d7ac24467afbc1669a896ea291
胡伟 <912826754@qq.com>
2019-07-04 09:20:37 +0800

    7.4

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -2,15 +2,33 @@
   @import './login.less';
 </style>
 
+
 <template>
   <div class="login">
     <div class="login-con">
-      <Card icon="log-in" title="欢迎登录" :bordered="false">
-        <div class="form-con">
-          <login-form @on-success-valid="handleSubmit"></login-form>
-          <p class="login-tip">输入任意用户名和密码即可</p>
-        </div>
-      </Card>
+      <Row class="height-100">
+        <Col span="16" class="height-100 login-bg">
+          <div class="login-company" onselectstart="return false;">
+            <img src="../../assets/images/logo.png" alt ondragstart="return false;">
+            <span>一体化管理平台（OMP）</span>
+          </div>
+        </Col>
+        <Col span="8" class="height-100" style="padding:50px;">
+          <h2 class="h2-title">河南油田工程咨询股份有限公司</h2>
+          <Card icon="log-in" :bordered="false" style="width:90%;margin:0 auto;">
+            <h2 style="font-weight:600; color:black;padding-bottom:20px;">登 录 到 你 的 账 户</h2>
+            <div class="form-con">
+              <login-form @on-success-valid="handleSubmit"></login-form>
+              <p class="login-tip"></p>
+            </div>
+          </Card>
+          <div class="copy-right">
+            <p>Copyright © 2019 河南油田工程咨询股份有限公司.All Rights Reserved</p>
+            <p>技术支持单位：河南油田工程咨询股份有限公司</p>
+            <p>支持电话：0371-53608335</p>
+          </div>
+        </Col>
+      </Row>
     </div>
   </div>
 </template>
